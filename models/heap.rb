0a49a355e862720c7c437d610c23d2773f1ab74c
Aaron Quint <aaron@quirkey.com>
2013-11-14 18:17:45 -0500

    Buggy most references

--- models/heap.rb
+++ models/heap.rb
@@ -5,4 +5,18 @@ class Heap < ActiveRecord::Base
     heap_entries.where("#{field} > 0").order(field => :desc).limit(15)
   end
 
+  def most_references
+    HeapEntry.find_by_sql [%{
+      SELECT heap_entries.*,
+      COUNT(1) AS num_references
+      FROM heap_entries
+      INNER JOIN heap_references ON
+      heap_references.heap_id = heap_entries.heap_id AND
+      heap_references.from_address = heap_entries.heap_address
+      GROUP BY heap_entries.id
+      ORDER by num_references DESC
+      LIMIT 10
+    }]
+  end
+
 end
